fe181d7abb36b538010d0ee8a20e21edfff26dc8
Ishan1818 <72986416+Ishan1818@users.noreply.github.com>
2021-01-21 14:58:27 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,58 @@
+var dog, dogimg, dogimg2;
+var database;
+var FoodStk, fs;
+function preload()
+{
+dogimg=loadImage("images/dogImg.png")
+dogimg2=loadImage("images/dogImg1.png")
+
+}
+
+function setup() {
+  database=firebase.database()
+	createCanvas(800, 700);
+dog=createSprite(700, 500, 20, 20)
+ dog.addImage(dogimg) 
+dog.scale=0.2;
+
+FoodStk=database.ref('FOOD')
+FoodStk.on("value", readStk)
+}
+
+
+function draw() {  
+  background("Orange")
+if (keyWentDown(UP_ARROW)){
+  writeStock(fs)
+  dog.addImage(dogimg2)
+}
+fill ("red")
+textSize(15)
+text("REMAINING FOOD---"+ fs, 200, 200)
+text("Please press the up arrow key to feed the dog", 200, 50)
+
+
+
+  drawSprites();
+  }
+function readStk(data){
+fs=data.val();
+  
+}
+function writeStock(x){
+if(x<=0){
+  x=0;
+  
+}else{
+  x=x-1;
+}
+database.ref('/').update({
+  FOOD:x
+  
+})
+}
+
+
+
+
+
